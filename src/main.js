26d290a326c0d35aa9e2771d3c5aea0cf9699c1e
frohlichramon01 <frohlichramon01@gmail.com>
2023-12-12 21:39:05 -0300

    mobile

--- src/main.js
+++ src/main.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(){
     const buttons = document.querySelectorAll('[data-tab-button]');
-    const questions = document.querySelectorAll('[data-faq-question]')
-    
+    const questions = document.querySelectorAll('[data-faq-question]');
 
+    const heroSection = document.querySelector('.hero');
+    const alturaHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function(){
+        const posicaoAtual = window.scrollY;
+
+        if (posicaoAtual < alturaHero) {
+            hideHeaderElements();
+        } else{
+            showHeaderElements();
+        }
+    })
+
+    //Programação das abas
     for (let i=0; i< buttons.length; i++){
         buttons[i].addEventListener("click", function(botao){
             const activeTab = botao.target.dataset.tabButton;
@@ -14,6 +27,7 @@ document.addEventListener("DOMContentLoaded", function(){
         })
     }
 
+    // FAQ Accordion 
     for (let i = 0; i < questions.length; i++){
         questions[i].addEventListener("click", abreFechaResposta);
     }
@@ -39,4 +53,14 @@ function hideAllTabs(){
     for (let i = 0; i< tabsContainer.length; i++){
         tabsContainer[i].classList.remove("shows__list--is-active");
     }
+}
+
+function hideHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
 }
